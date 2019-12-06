4a445cae67a49c0d438faa96df4838a92a168afe
LCVClass <keisuke.sato@lasallecollegevancouver.com>
2019-12-06 12:41:15 -0800

    -Adding Match3

---
+++ Assets/Board.cs
@@ -0,0 +1,67 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+public class Board : MonoBehaviour
+{
+    public List<Sprite> iconList;
+    bool isSwaping;
+    public float speed;
+    public int numColumns;
+    public int numRows;
+    private GameObject[,] tiles;
+    public GameObject tile;
+    public float xOffset;
+    public float yOffset;
+
+    private void Start()
+    {
+        GenerateMap();
+    }
+    void GenerateMap()
+    {
+        tiles = new GameObject[numColumns, numRows];
+        Sprite previousLeft = null;//These are for duplicates when map generation
+        Sprite below = null;                //These are for duplicates when map generation
+        if (numColumns > 0 && numRows > 0)
+        {
+            for (int y = 0; y < numRows; ++y)
+            {
+                for (int x = 0; x < numColumns; ++x)
+                {
+                    var deltaPos = new Vector3(transform.position.x + (xOffset * x), transform.position.y + (yOffset * y));
+                    tiles[x, y] = Instantiate(tile, deltaPos, Quaternion.identity, gameObject.transform);
+                    List<Sprite> possibleCharacters = new List<Sprite>();
+                    possibleCharacters.AddRange(iconList); // add the iconlist elements into possibleCharacters;
+
+                    if(x > 0)
+                        below = tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite;
+                    if (y > 0)
+                        below = tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite;
+                    if (previousLeft)
+                        possibleCharacters.Remove(previousLeft);
+                    if(below)
+                        possibleCharacters.Remove(below);
+                    
+                    Sprite newSprite = possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+                    tiles[x,y].GetComponent<SpriteRenderer>().sprite = newSprite;
+                    //if(newSprite != null)
+                    //    previousLeft = newSprite;
+                } 
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isSwaping)
+        {
+            //float step = speed * Time.deltaTime; // calculate distance to move
+            //milk.transform.position = Vector3.Lerp(milk.transform.position, toChocolate, step);
+            //chocolote.transform.position = Vector3.Lerp(chocolote.transform.position, toMilk, step);
+        }
+    }
+}
---
+++ Assets/Match 3 Starter/Scripts/Board and Grid/Tile.cs
@@ -0,0 +1,90 @@
+﻿//https://www.raywenderlich.com/673-how-to-make-a-match-3-game-in-unity
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Tile : MonoBehaviour
+{
+    private static Color selectedColor = new Color(.5f, .5f, .5f, 1.0f);
+    private static Tile previousSelected = null;
+
+    private SpriteRenderer render;
+    private bool isSelected = false;
+
+    private Vector2[] adjacentDirections = new Vector2[] { Vector2.up, Vector2.down, Vector2.left, Vector2.right };
+    bool isSwaping;
+    Vector3 currentStartingPos;
+    Vector3 tileToMoveWithStartingPos;
+    GameObject tileToMoveWith;
+    public float speed;
+    void Awake()
+    {
+        render = GetComponent<SpriteRenderer>();
+        isSwaping = false;
+    }
+
+    private void Select()
+    {
+        isSelected = true;
+        render.color = selectedColor;
+        previousSelected = gameObject.GetComponent<Tile>();
+    }
+
+    private void Deselect()
+    {
+        isSelected = false;
+        render.color = Color.white;
+        previousSelected.isSelected = false;
+        previousSelected.render.color = Color.white;
+        previousSelected = null;
+    }
+
+    private void OnMouseDown() //If Clicked
+    {
+        if (render.sprite == null || isSwaping)
+            return;
+        if (isSelected)
+            Deselect();
+        else
+        {
+            if (previousSelected == null)
+                Select();
+            else
+            {
+                SwapSprite(previousSelected);
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if (isSwaping)
+        {
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, tileToMoveWithStartingPos, step);
+            tileToMoveWith.transform.position = Vector3.MoveTowards(tileToMoveWith.transform.position, currentStartingPos, step);
+            //Have tile arrive to their destination
+            if (transform.position.x == tileToMoveWithStartingPos.x  && transform.position.y == tileToMoveWithStartingPos.y &&
+                tileToMoveWith.transform.position.x == currentStartingPos.x && tileToMoveWith.transform.position.y == currentStartingPos.y)
+            {
+                isSwaping = false;
+                Deselect();
+            }
+        }
+    }
+
+    public void SwapSprite(Tile b)
+    {
+        //if (render.sprite == b.GetComponent<SpriteRenderer>().sprite)
+        //    return;
+
+        //Populating variables for swapping
+        if (!isSwaping)
+        {
+            tileToMoveWithStartingPos =  b.transform.position;
+            tileToMoveWith = b.gameObject;
+            currentStartingPos = transform.position;
+            isSwaping = true;
+        }
+    }
+}
